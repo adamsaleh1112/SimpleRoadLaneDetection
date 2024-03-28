078b5e5b8cc7fc69df04eb18f001a8c80b91e372
Adam <144299219+adamsaleh1112@users.noreply.github.com>
2024-03-28 08:41:34 -0400

    Update main.py

--- main.py
+++ main.py
@@ -34,32 +34,81 @@ while (True):
     cv2.circle(result_for_adding, (200,400), 250, (0,0,0), 500)
 
     # CRUDE LINE DETECTION
-    lines = cv2.HoughLinesP(edges, rho=1, theta=np.pi / 180, threshold=50, minLineLength=5, maxLineGap=150)
+    lines = cv2.HoughLinesP(edges, rho=1, theta=np.pi / 180, threshold=50, minLineLength=5, maxLineGap=250)
 
     if lines is not None:
 
         right_lines = []
         left_lines = []
+        slopes_left = []
+        slopes_right = []
+        valid_lines = []
 
         for line in lines:
             x1, y1, x2, y2 = line[0]
 
             slope = (y2-y1) / ((x2-x1) + 1) # +1 prevents denominator from being 0
             abs_slope = abs(slope) + 0.01 # +.01 to prevent slope from being inf
+            slope_degrees = np.rad2deg(np.arctan(slope))
 
-            x1_true = max(x1, x2) # making x1 and y1 always on bottom
-            x2_true = min(x1, x2)
-            y1_true = max(y1, y2)
-            y2_true = min(y1, y2)
+            if slope_degrees > 75 or slope_degrees < -75:
+                valid_lines.append(line)
 
-            bottom_y_value = y1_true
-            distance_to_bottom = 640 - bottom_y_value
-            extended_x_value = int(x1_true + (x1_true/abs_slope))
-            extended_y_value = 640
+        for line in valid_lines:
+            x1, y1, x2, y2 = line[0]
+            cv2.line(result, (x1, y1), (x2, y2), (0, 0, 255), 5)
 
-            #cv2.line(result_for_adding, (x1, y1), (x2, y2), (0, 0, 255), 5)
-            cv2.line(result, (extended_x_value, extended_y_value), (x2_true, y2_true), (0, 0, 255), 5)
-            cv2.circle(result, (x1_true, y1_true), 3, (255, 255, 255), 5)
+            if x1 > 0 and x1 < 150:
+                left_lines.append(line)
+            if x1 > 325 and x1 < 475:
+                right_lines.append(line)
+
+        for line in right_lines:
+            x1, y1, x2, y2 = line[0]
+            slope = (y2 - y1) / ((x2 - x1) + 1)
+            slopes_right.append(slope)
+
+        for line in left_lines:
+            x1, y1, x2, y2 = line[0]
+            slope = (y2 - y1) / ((x2 - x1) + 1)
+            slopes_left.append(slope)
+
+        avg_slopes_left = sum(slopes_left) / (len(slopes_left) + 0.01)
+        avg_slopes_right = sum(slopes_right) / (len(slopes_right) + 0.01)
+
+        print(np.rad2deg(np.arctan(avg_slopes_left)))
+
+            # x1_true = max(x1, x2) # making x1 and y1 always on bottom
+            # x2_true = min(x1, x2)
+            # y1_true = max(y1, y2)
+            # y2_true = min(y1, y2)
+            #
+            # bottom_y_value = y1_true
+            #
+            # #cv2.line(result_for_adding, (x1, y1), (x2, y2), (0, 0, 255), 5)
+            # cv2.line(result, (x1_true, y1_true), (x2_true, y2_true), (0, 0, 255), 5)
+            # cv2.circle(result, (x1_true, y1_true), 3, (255, 255, 255), 5)
+
+            # if x1_true < 150 and x1_true > 0:
+            #     left_lines.append(line)
+            # if x1_true < 475 and x1_true > 325:
+            #     right_lines.append(line)
+            # else:
+            #     pass
+            #
+            # for line in left_lines:
+            #     lx1, ly1, lx2, ly2 = line[0]
+            #     #bottom_x
+            #     slope_left = (ly2-ly1) / ((lx2-lx1) + 1)
+            #     slopes_left.append(slope_left)
+            #
+            # for line in right_lines:
+            #     rx1, ry1, rx2, ry2 = line[0]
+            #     slope_right = (ry2-ry1) / ((rx2-rx1) + 1)
+            #     slopes_right.append(slope_right)
+            #
+            # avg_slope_left = sum(slopes_left) / (len(slopes_left) + .01)
+            # avg_slope_right = sum(slopes_right) / (len(slopes_right) + .01)
 
 
         # EXTEND LINES TO TOP AND BOTTOM OF FRAME
